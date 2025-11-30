b9548e36e472f3e879c950db25b04ba0bc219f0e
Weslley Araujo <weslleyaraujo9917@gmail.com>
2025-11-30 01:22:55 -0300

    Fix millions of games generating

--- public/game/tk-bridge.js
+++ public/game/tk-bridge.js
@@ -41,9 +41,24 @@ function loadGames(startId, endId, options = {}) {
 // Main game list function
 window.getGameList = () => {
     const allGames = [
-        // 10000 series games (100000-1000100)
-        ...loadGames(100000, 1000100),
-       
+        // 100000 series games (100000-100100)
+        ...loadGames(100000, 100100),
+        // 200000 series games (200000-200100)
+        ...loadGames(200000, 200100),
+        // 300000 series games (300000-300100)
+        ...loadGames(300000, 300100),
+        // 400000 series games (400000-400100)
+        ...loadGames(400000, 400100),
+        // 500000 series games (500000-500100)
+        ...loadGames(500000, 500100),
+        // 600000 series games (600000-600100)
+        ...loadGames(600000, 600100),
+        // 700000 series games (700000-700100)
+        ...loadGames(700000, 700100),
+        // 800000 series games (800000-800100)
+        ...loadGames(800000, 800100),
+        // 900000 series games (900000-900100)
+        ...loadGames(900000, 900100),
     ];
     
     return allGames;
